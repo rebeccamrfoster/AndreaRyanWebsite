dca28db4b5e9befbc606f1ee31aa37cbc226f972
rebeccamrfoster <rebeccamrfoster@gmail.com>
2022-01-17 22:55:53 -0500

    Experiment with php email processing file

--- process.php
+++ process.php
@@ -1,24 +1,39 @@
 <?php
 
-    $to = "rebeccamrfoster@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $headers = "From: $from";
-    $subject = "You have a message.";
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $to = "rebeccamrfoster@gmail.com";
+        $from = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"phone"} = "phone";
-    $fields{"message"} = "message";
+        $headers = "From: $from";
+        $subject = "You have received a message.";
+        // $txt = "You have a message from".$name".\n\n".$message;
+
+        mail($to, $subject, $message, $headers);
+        header("Location: process.php?mailsend");
+    }
+
+    // $to = "rebeccamrfoster@gmail.com";
+    // $from = $_REQUEST['email'];
+    // $name = $_REQUEST['name'];
+    // $headers = "From: ".$from;
+    // $subject = "You have a message.";
+
+    // $fields = array();
+    // $fields{"name"} = "name";
+    // $fields{"email"} = "email";
+    // $fields{"phone"} = "phone";
+    // $fields{"message"} = "message";
 
     
 
-    $body = "Here is what was sent:\r\n"; 
+    // $body = "Here is what was sent:\r\n"; 
 
-    foreach($fields as $a => $b){$body .= $b." : ".$_REQUEST[$a]."\r\n"; }
+    // foreach($fields as $a => $b){$body .= $b." : ".$_REQUEST[$a]."\r\n"; }
 
 
-    $send = mail($to, $subject, $body, $headers);
+    // $send = mail($to, $subject, $body, $headers);
 
 ?>
